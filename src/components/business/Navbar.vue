9da0b5dbbdf31a8917d373476ba2b05a79d71edd
Ahmed Ashraf <a.ashraf.elbendary@gmail.com>
2025-02-07 00:31:02 +0300

    Complete Base Design

--- src/components/business/Navbar.vue
+++ src/components/business/Navbar.vue
@@ -1,60 +1,134 @@
 <style lang="scss" scoped>
 .navbar {
-  &__menu {
-    list-style: none;
+  padding: 16px 80px;
 
-    &__item {
-      color: var(--accent-color);
+  // Org logo
+
+  &__org-logo-container {
+    max-width: 86px;
+    width: 86px;
+    &__img {
+      width: 100%;
     }
   }
-}
 
-.activeLink {
+  // Menue Items
+
+  &__menu {
+    list-style: none;
+    &__item {
+      padding-right: 10px;
+      &__link {
+        text-decoration: none;
+        color: var(--accent-color);
+        &.activeLink {
+          color: var(--primary-color);
+        }
+      }
+    }
+    &__item:last-child {
+      padding: 0;
+    }
+  }
+
+  &__collapsed-menu.v-btn {
+    height: auto;
+    padding: 0;
+    box-shadow: none;
+    min-width: unset;
+  }
 }
 </style>
 
-<script setup>
-import { RouterLink, useRoute } from "vue-router";
+<script lang="ts" setup>
+import { useRoute } from "vue-router";
 import logo from "@/assets/images/logo.png";
+import heartIcon from "@/assets/icons/heart.png";
+import bagIcon from "@/assets/icons/bag.png";
+import menuIcon from "@/assets/icons/menu-icon.png";
+import NavbarMenuItem from "../ui/NavbarMenuItem.vue";
+import type { NavbarItem } from "@/interfaces/navbar.interface";
 
-const isActiveLink = (routePath) => {
+const menuItemsList: NavbarItem[] = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "All Category",
+    path: "/categories",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+];
+
+const isActiveLink = (routePath: string) => {
   const route = useRoute();
   return route.path === routePath;
 };
 </script>
 
 <template>
-  <nav class="navbar">
-    <v-img :width="86" aspect-ratio="1/1" cover :src="logo"></v-img>
+  <nav class="navbar d-flex justify-space-between align-center">
+    <figure class="navbar__org-logo-container">
+      <v-img
+        class="navbar__org-logo-container__img"
+        aspect-ratio="1/1"
+        cover
+        :src="logo"
+      ></v-img>
+    </figure>
     <ul class="d-none d-md-flex navbar__menu">
-      <li class="navbar__menu__item">
-        <RouterLink
-          to="/"
-          :class="`navbar__menu__item__link ${
-            isActiveLink('/') ? 'activeLink' : ''
-          }`"
-          >Home</RouterLink
-        >
-      </li>
-      <li>
-        <RouterLink
-          to="/categories"
-          :class="`${isActiveLink('/categories') ? '' : ''}`"
-          >All Category</RouterLink
-        >
-      </li>
-      <li>
-        <RouterLink to="/about" :class="`${isActiveLink('/about') ? '' : ''}`"
-          >About</RouterLink
-        >
-      </li>
-      <li>
-        <RouterLink
-          to="/contact"
-          :class="`${isActiveLink('/contact') ? '' : ''}`"
-          >Contact</RouterLink
-        >
+      <li v-for="item in menuItemsList" class="navbar__menu__item">
+        <NavbarMenuItem
+          :navigateTo="item.path"
+          classes="navbar__menu__item__link"
+          activeClass="activeLink"
+          :isActive="isActiveLink(item.path)"
+          :linkTitle="item.title"
+        />
       </li>
     </ul>
+    <ul class="navbar__options d-flex justify-space-center align-center">
+      <figure class="navbar__options__fav pr-5">
+        <v-img :width="20" aspect-ratio="1/1" cover :src="heartIcon"></v-img>
+      </figure>
+      <figure class="navbar__options__cart">
+        <v-img :width="20" aspect-ratio="1/1" cover :src="bagIcon"></v-img>
+      </figure>
+    </ul>
+
+    <v-btn class="navbar__collapsed-menu d-flex d-md-none" color="">
+      <figure class="collapsed-menu__icon-box">
+        <v-img
+          class="collapsed-menu__icon-box__img"
+          :width="20"
+          aspect-ratio="1/1"
+          cover
+          :src="menuIcon"
+        ></v-img>
+      </figure>
+      <v-menu activator="parent">
+        <v-list>
+          <v-list-item>
+            <v-list-item-title v-for="item in menuItemsList">
+              <NavbarMenuItem
+                :navigateTo="item.path"
+                classes="navbar__menu__item__link"
+                activeClass="activeLink"
+                :isActive="isActiveLink(item.path)"
+                :linkTitle="item.title"
+              />
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-btn>
   </nav>
 </template>
---
+++ src/components/ui/NavbarMenuItem.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink } from "vue-router";
+const props = defineProps({
+  linkTitle: String,
+  navigateTo: String,
+  isActive: Boolean,
+  activeClass: String,
+  classes: String,
+});
+</script>
+
+<template>
+  <RouterLink
+    :to="navigateTo"
+    :class="`${classes} ${isActive ? activeClass : ''}`"
+    >{{ linkTitle }}</RouterLink
+  >
+</template>
